104aff91f7c8080d715d2b6407b556097ec9272e
RSWeslei <wrweslei@unochapeco.edu.br>
2022-07-29 18:30:32 -0300

    3 resolvidas

--- 2161.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('./dev/stdin.txt', 'utf8');
-var lines = input.split('\n');
-
-const pegarValores = (line) => line.split(" ").map(a => Number(a));
-
-let n = lines.shift()
-
-console.log((3+(1/(6+1/6))).toFixed(10));
